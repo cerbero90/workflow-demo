913e0ab43d370f6c4d4d7eca173058a1c5cb99f6
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-27 19:57:08 +0200

    Create welcome email

---
+++ app/views/emails/welcome.blade.php
@@ -0,0 +1,9 @@
+<body>
+	<h1>Welcome {{$name}}!</h1>
+
+	<p>Thank you for joining us! Please review your data.</p>
+	<ul>
+		<li>Your email: {{$email}}</li>
+		<li>Your password: {{$password}}</li>
+	</ul>
+</body>
