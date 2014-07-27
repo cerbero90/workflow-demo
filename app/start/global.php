863ff12450704f26789ce41fb30533e662b658fa
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-27 19:50:39 +0200

    Register displayable exception

--- app/start/global.php
+++ app/start/global.php
@@ -17,6 +17,7 @@ ClassLoader::addDirectories(array(
 	app_path().'/controllers',
 	app_path().'/models',
 	app_path().'/database/seeds',
+	app_path().'/exceptions',
 
 ));
 
@@ -49,6 +50,11 @@ Log::useFiles(storage_path().'/logs/laravel.log');
 App::error(function(Exception $exception, $code)
 {
 	Log::error($exception);
+});
+
+App::error(function(DisplayableException $exception)
+{
+	return Redirect::back()->withInput()->withErrors($exception->getMessage());
 });
 
 /*
