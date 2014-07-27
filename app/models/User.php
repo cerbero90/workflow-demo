52ac7abf1f0bb9ddd85391f5699239b74e848923
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-27 19:52:43 +0200

    Set mass-assignable attributes

--- app/models/User.php
+++ app/models/User.php
@@ -23,4 +23,10 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+	/**
+	 * @author	Andrea Marco Sartori
+	 * @var		array	$fillable	Mass-assignable properties.
+	 */
+	protected $fillable = array('name', 'email', 'password');
+
 }
