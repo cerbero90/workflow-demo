8d185d9d248a4c64e4cec44df0fe1cd229d2fa6a
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-07-27 19:52:03 +0200

    Configure sender

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'demo@example.com', 'name' => 'Andrea Marco Sartori'),
 
 	/*
 	|--------------------------------------------------------------------------
