2e8e11c874599ef864829af8a5793a83cf4844e9
Andrea Marco Sartori <22cerbero22@gmail.com>
2015-02-21 17:36:09 +0100

    Override registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::resource('auth/register', 'RegisterController', ['only' => ['index', 'store']]);
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
