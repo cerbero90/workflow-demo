3bd2b29187c073fdc60bcf8a723ca74d60495761
Andrea Marco Sartori <22cerbero22@gmail.com>
2015-02-21 18:57:23 +0100

    Create controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,30 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+}
